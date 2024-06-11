a2c3d75b31b51f1537ac1e5f821611e61eb8282d
jvnagaoo <127306277+jvnagaoo@users.noreply.github.com>
2024-06-11 16:16:46 -0300

    Add files via upload

---
+++ Js.main.js
@@ -0,0 +1,48 @@
+const caixaPrincipal =document.querySelector('.caixa-principal');
+const caixaPerguntas =document.querySelector('.caixa-alternativas');
+const caixaAlternativas =document.querySelector('.caixa-alternativas');
+const caixaResultado =document.querySelector('.caixa-resultado');
+const textoResultado =document.querySelector('.caixa-resultado');
+
+  const perguntas= [
+    {
+        enunciado:"Brasil é conhecido por mundialmente por ser o país do... ?" ,
+        alternativa:[
+            'Volei',
+            "Fuetebol"
+        ]
+    },
+  
+    {
+        enunciado:"Qual a fruta mais consumida no brasil ?" ,
+        alternativa:[
+            'Banana',
+            "Morango "
+        ]
+    },
+  
+    {
+        enunciado:"Qual é a maior festa popular do Brasil, que mistura canto e dança do samba e desfiles de escolas de samba?" ,
+        alternativa:[
+            'Natal',
+            "carnaval"
+        ]
+    },
+  
+    {
+        enunciado:"O frevo é uma dança típica de qual estado do Brasil?" ,
+        alternativa:[
+            'Pernanbuco',
+            "Bahia" 
+        ]
+    },
+
+    {
+        enunciado:"Qual destes personagens do folclore brasileiro tem os cabelos vermelhos e pés ao inverso ?" ,
+        alternativa:[
+            'Mula sem cabeça',
+            "Curupira"
+        ] 
+    }]
+  
+  
